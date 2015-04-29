11e4541da736c18c904387f1246b1e6a7e8baccf
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2015-04-29 18:38:25 +0200

    build

--- build/simple-color-picker.js
+++ build/simple-color-picker.js
@@ -3505,7 +3505,7 @@ function restParam(func, start) {
 module.exports = restParam;
 
 },{}],29:[function(require,module,exports){
-var css = ".scp {\n  width: 175px;\n  height: 150px;\n  background: #808080;\n  padding: 5px;\n  -webkit-user-select: none;\n  -moz-user-select: none;\n  -ms-user-select: none;\n  user-select: none;\n}\n.scp .saturation {\n  position: relative;\n  width: 150px;\n  height: 150px;\n  background: linear-gradient(to right, #fff 0%, #f00 100%);\n  float: left;\n  margin-right: 5px;\n}\n.scp .brightness {\n  width: 100%;\n  height: 100%;\n  background: linear-gradient(to top, #000 0%, rgba(255,255,255,0) 100%);\n}\n.scp .sb-selector {\n  border: 2px solid;\n  border-color: #fff;\n  position: absolute;\n  width: 10px;\n  height: 10px;\n  background: #fff;\n  border-radius: 10px;\n  top: -5px;\n  left: -5px;\n}\n.scp .hue {\n  width: 20px;\n  height: 150px;\n  background: #00f;\n  position: relative;\n  float: left;\n  background: linear-gradient(to bottom, #f00 0%, #f0f 17%, #00f 34%, #0ff 50%, #0f0 67%, #ff0 84%, #f00 100%);\n}\n.scp .h-selector {\n  position: absolute;\n  width: 100%;\n}\n.scp .h-selector .right {\n  width: 0;\n  height: 0;\n  border-style: solid;\n  position: absolute;\n}\n.scp .h-selector .right {\n  right: -3px;\n  background: #fff;\n  width: 10px;\n  height: 2px;\n  border: none;\n  border-bottom: 1px solid #000;\n}\n"; (require("browserify-css").createStyle(css, { "href": "src/css/style.css"})); module.exports = css;
+var css = ".scp {\n  width: 175px;\n  height: 150px;\n  padding: 5px;\n  -webkit-user-select: none;\n  -moz-user-select: none;\n  -ms-user-select: none;\n  user-select: none;\n  position: relative;\n}\n.scp .saturation {\n  position: relative;\n  width: calc(100% - 25px);\n  height: 100%;\n  background: linear-gradient(to right, #fff 0%, #f00 100%);\n  float: left;\n  margin-right: 5px;\n}\n.scp .brightness {\n  width: 100%;\n  height: 100%;\n  background: linear-gradient(to top, #000 0%, rgba(255,255,255,0) 100%);\n}\n.scp .sb-selector {\n  border: 2px solid;\n  border-color: #fff;\n  position: absolute;\n  width: 10px;\n  height: 10px;\n  background: #fff;\n  border-radius: 10px;\n  top: -5px;\n  left: -5px;\n}\n.scp .hue {\n  width: 20px;\n  height: 100%;\n  background: #00f;\n  position: relative;\n  float: left;\n  background: linear-gradient(to bottom, #f00 0%, #f0f 17%, #00f 34%, #0ff 50%, #0f0 67%, #ff0 84%, #f00 100%);\n}\n.scp .h-selector {\n  position: absolute;\n  width: 100%;\n}\n.scp .h-selector .right {\n  width: 0;\n  height: 0;\n  border-style: solid;\n  position: absolute;\n}\n.scp .h-selector .right {\n  right: -3px;\n  background: #fff;\n  width: 10px;\n  height: 2px;\n  border: none;\n  border-bottom: 1px solid #000;\n}\n"; (require("browserify-css").createStyle(css, { "href": "src/css/style.css"})); module.exports = css;
 },{"browserify-css":1}],30:[function(require,module,exports){
 'use strict';
 
@@ -3516,6 +3516,7 @@ var Emitter = require('component-emitter');
 var isNumber = require('is-number');
 var offset = require('./utils/dom/offset');
 var clamp = require('./utils/maths/clamp');
+var createColor = require('./utils/createColor');
 var css = require('./css/style.css');
 
 
@@ -3523,32 +3524,30 @@ function SimpleColorPicker(options) {
 
   // options
   options = options || {};
+  this.color = null;
+  this._hueColor = null;
+  this.callbacks = [];
 
   // bind methods to scope (only if needed)
-  bindAll(this, 'onSaturationMouseMove', 'onSaturationMouseDown', 'onSaturationMouseUp', 'onHueMouseDown', 'onHueMouseUp', 'onHueMouseMove');
+  bindAll(this, '_onSaturationMouseMove', '_onSaturationMouseDown', '_onSaturationMouseUp', '_onHueMouseDown', '_onHueMouseUp', '_onHueMouseMove');
 
   // dom template
-  this.template = [
-    '<div class="scp">',
-      '<div class="saturation">',
-        '<div class="brightness"></div>',
-        '<div class="sb-selector"></div>',
-      '</div>',
-      '<div class="hue">',
-        '<div class="h-selector">',
-          '<div class="right"></div>',
-        '</div>',
+  this._template = [
+    '<div class="saturation">',
+      '<div class="brightness"></div>',
+      '<div class="sb-selector"></div>',
+    '</div>',
+    '<div class="hue">',
+      '<div class="h-selector">',
+        '<div class="right"></div>',
       '</div>',
     '</div>'
   ].join('\n');
 
-  
   // dom elements
   this.$el = document.createElement('div');
-  this.$el.innerHTML = this.template;
-  if(options.el) {
-    this.appendTo(options.el);
-  }
+  this.$el.className = 'scp';
+  this.$el.innerHTML = this._template;
 
   this.$saturation = this.$el.querySelector('.saturation');
   this.$hue = this.$el.querySelector('.hue');
@@ -3556,11 +3555,25 @@ function SimpleColorPicker(options) {
   this.$hSelector = this.$el.querySelector('.h-selector');
 
   // create event listeners
-  this.$saturation.addEventListener('mousedown', this.onSaturationMouseDown);
-  this.$hue.addEventListener('mousedown', this.onHueMouseDown);
+  this.$saturation.addEventListener('mousedown', this._onSaturationMouseDown);
+  this.$hue.addEventListener('mousedown', this._onHueMouseDown);
+
+  // some styling and doming from options
+  if(options.el) {
+    this.appendTo(options.el);
+  }
+  if(options.background) {
+    this.$el.style.background = createColor(options.background).hexString();
+  }
+  if(options.width && isNumber(options.width)) {
+    this.$el.style.width = options.width + 'px';
+  }
+  if(options.height && isNumber(options.width)) {
+    this.$el.style.height = options.height + 'px';
+  }
   
   setTimeout(function() {
-    this.setColor(options.color || '#FF0000');
+    this.setColor(options.color);
   }.bind(this), 0);
 
   return this;
@@ -3569,24 +3582,20 @@ function SimpleColorPicker(options) {
 Emitter(SimpleColorPicker.prototype);
 
 SimpleColorPicker.prototype.setColor = function(color) {
-  if(isNumber(color)) {
-    color = '#' + color.toString(16);
+  this.color = createColor(color);
+  this._hueColor = this.color.clone().saturation(100).lightness(50);
+  if(this.color.saturation() === 0) {
+    this._hueColor.red(255);
   }
-
-  this.color = Color(color || '#FF0000');
-  this.hueColor = this.color.clone().saturation(100).lightness(50);
-  if(this.color.red() === this.color.green() && this.color.red() == this.color.blue()) {
-    this.hueColor.red(255);
-  }
-  this.moveSelectorTo((this.color.saturationv() / 100) * (this.$saturation.offsetWidth - 5) + 5, (1 - (this.color.value() / 100)) * (this.$saturation.offsetHeight - 2) + 5);
-  this.moveHueTo((1 - (this.hueColor.hue() / 360)) * (this.$hue.offsetHeight - 2) + 4);
+  this._moveSelectorTo((this.color.saturationv() / 100) * (this.$saturation.offsetWidth - 5) + 5, (1 - (this.color.value() / 100)) * (this.$saturation.offsetHeight - 2) + 5);
+  this._moveHueTo((1 - (this._hueColor.hue() / 360)) * (this.$hue.offsetHeight - 2) + 4);
 };
 
 SimpleColorPicker.prototype.remove = function() {
-  this.$saturation.removeEventListener('mousedown', this.onSaturationMouseDown);
-  this.$hue.removeEventListener('mousedown', this.onHueMouseDown);
-  this.onSaturationMouseUp();
-  this.onHueMouseUp();
+  this.$saturation.removeEventListener('mousedown', this._onSaturationMouseDown);
+  this.$hue.removeEventListener('mousedown', this._onHueMouseDown);
+  this._onSaturationMouseUp();
+  this._onHueMouseUp();
   if(this.$el.parentNode) {
     this.$el.parentNode.removeChild(this.$el);
   }
@@ -3596,7 +3605,18 @@ SimpleColorPicker.prototype.appendTo = function(domElement) {
   domElement.appendChild(this.$el);
 };
 
-SimpleColorPicker.prototype.moveSelectorTo = function(x, y) {
+SimpleColorPicker.prototype.getHexString = function() {
+  return this.color.hexString();
+};
+
+SimpleColorPicker.prototype.onChange = function(callback) {
+  this.on('update', callback);
+};
+
+/* ============================================================================= 
+  "Private" Methods LOL silly javascript
+============================================================================= */
+SimpleColorPicker.prototype._moveSelectorTo = function(x, y) {
   var maxX = this.$saturation.offsetWidth - 5;
   var maxY = this.$saturation.offsetHeight - 5;
 
@@ -3606,7 +3626,7 @@ SimpleColorPicker.prototype.moveSelectorTo = function(x, y) {
   this.color.saturationv(100 * x / maxX);
   this.color.value(100 * (1 - (y / maxY)));
 
-  this.updateColor();
+  this._updateColor();
 
   transform(this.$sbSelector, {
     x: x,
@@ -3614,7 +3634,7 @@ SimpleColorPicker.prototype.moveSelectorTo = function(x, y) {
   });
 };
 
-SimpleColorPicker.prototype.moveHueTo = function(y) {
+SimpleColorPicker.prototype._moveHueTo = function(y) {
   var maxY = this.$hue.offsetHeight - 2;
   
   y = clamp(y - 4, 0, maxY);
@@ -3622,70 +3642,77 @@ SimpleColorPicker.prototype.moveHueTo = function(y) {
   var hue = 360 * (1 - (y / maxY));
 
   this.color.hue(hue);
-  this.hueColor.hue(hue);
-  this.updateHue();
+  this._hueColor.hue(hue);
+  this._updateHue();
 
   transform(this.$hSelector, {
     y: y
   });
 };
 
-SimpleColorPicker.prototype.updateHue = function() {
-  this.$saturation.style.background = 'linear-gradient(to right, #fff 0%, ' + this.hueColor.hexString() + ' 100%)';
-  this.updateColor();
+SimpleColorPicker.prototype._updateHue = function() {
+  this.$saturation.style.background = 'linear-gradient(to right, #fff 0%, ' + this._hueColor.hexString() + ' 100%)';
+  this._updateColor();
 };
 
-SimpleColorPicker.prototype.updateColor = function() {
+SimpleColorPicker.prototype._updateColor = function() {
   this.$sbSelector.style.background = this.color.hexString();
   this.$sbSelector.style.borderColor = this.color.dark() ? '#FFF' : '#000';
-  this.emit('update', this.getColor());
+  this.emit('update', this.getHexString());
 };
 
-SimpleColorPicker.prototype.getColor = function() {
-  return this.color.hexString();
-};
 
 /* ============================================================================= 
   Events
 ============================================================================= */
-SimpleColorPicker.prototype.onSaturationMouseDown = function(e) {
-  var saturationOffset = offset(this.$saturation);
-  this.moveSelectorTo(e.clientX - saturationOffset.left, e.clientY - saturationOffset.top);
-  window.addEventListener('mouseup', this.onSaturationMouseUp);
-  window.addEventListener('mousemove', this.onSaturationMouseMove);
+SimpleColorPicker.prototype._onSaturationMouseDown = function(e) {
+  var sbOffset = offset(this.$saturation);
+  this._moveSelectorTo(e.clientX - sbOffset.left, e.clientY - sbOffset.top);
+  window.addEventListener('mouseup', this._onSaturationMouseUp);
+  window.addEventListener('mousemove', this._onSaturationMouseMove);
   e.preventDefault();
 };
 
-SimpleColorPicker.prototype.onSaturationMouseMove = function(e) {
-  var saturationOffset = offset(this.$saturation);
-  this.moveSelectorTo(e.clientX - saturationOffset.left, e.clientY - saturationOffset.top);
+SimpleColorPicker.prototype._onSaturationMouseMove = function(e) {
+  var sbOffset = offset(this.$saturation);
+  this._moveSelectorTo(e.clientX - sbOffset.left, e.clientY - sbOffset.top);
 };
 
-SimpleColorPicker.prototype.onSaturationMouseUp = function(e) {
-  window.removeEventListener('mouseup', this.onSaturationMouseUp);
-  window.removeEventListener('mousemove', this.onSaturationMouseMove);
+SimpleColorPicker.prototype._onSaturationMouseUp = function(e) {
+  window.removeEventListener('mouseup', this._onSaturationMouseUp);
+  window.removeEventListener('mousemove', this._onSaturationMouseMove);
 };
 
-SimpleColorPicker.prototype.onHueMouseDown = function(e) {
-  var hueOffset = offset(this.$hue);
-  this.moveHueTo(e.clientY - hueOffset.top);
-  window.addEventListener('mouseup', this.onHueMouseUp);
-  window.addEventListener('mousemove', this.onHueMouseMove);
+SimpleColorPicker.prototype._onHueMouseDown = function(e) {
+  var hOffset = offset(this.$hue);
+  this._moveHueTo(e.clientY - hOffset.top);
+  window.addEventListener('mouseup', this._onHueMouseUp);
+  window.addEventListener('mousemove', this._onHueMouseMove);
   e.preventDefault();
 };
 
-SimpleColorPicker.prototype.onHueMouseMove = function(e) {
-  var hueOffset = offset(this.$hue);
-  this.moveHueTo(e.clientY - hueOffset.top);
+SimpleColorPicker.prototype._onHueMouseMove = function(e) {
+  var hOffset = offset(this.$hue);
+  this._moveHueTo(e.clientY - hOffset.top);
 };
 
-SimpleColorPicker.prototype.onHueMouseUp = function(e) {
-  window.removeEventListener('mouseup', this.onHueMouseUp);
-  window.removeEventListener('mousemove', this.onHueMouseMove);
+SimpleColorPicker.prototype._onHueMouseUp = function(e) {
+  window.removeEventListener('mouseup', this._onHueMouseUp);
+  window.removeEventListener('mousemove', this._onHueMouseMove);
 };
 
 module.exports = SimpleColorPicker;
-},{"./css/style.css":29,"./utils/dom/offset":31,"./utils/maths/clamp":32,"color":2,"component-emitter":7,"dom-transform":8,"is-number":13,"lodash.bindall":14}],31:[function(require,module,exports){
+},{"./css/style.css":29,"./utils/createColor":31,"./utils/dom/offset":32,"./utils/maths/clamp":33,"color":2,"component-emitter":7,"dom-transform":8,"is-number":13,"lodash.bindall":14}],31:[function(require,module,exports){
+var isNumber = require('is-number');
+var Color = require('color');
+
+module.exports = function createColor(color) {
+  if(isNumber(color)) {
+    color = '#' + color.toString(16);
+  }
+  return Color(color || '#FFFFFF');
+};
+},{"color":2,"is-number":13}],32:[function(require,module,exports){
 module.exports = function getOffset(elem) {
   var offset = {left: 0, top:0};
   if (elem.offsetParent) {
@@ -3696,7 +3723,7 @@ module.exports = function getOffset(elem) {
   }
   return offset;
 };
-},{}],32:[function(require,module,exports){
+},{}],33:[function(require,module,exports){
 module.exports = function(value, min, max) {
   return Math.min(Math.max(value, min), max);
 }
