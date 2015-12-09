0573379ec22fef28511ebbafe9f939aa98ee4c1e
Eugene Cheung <echeung@infusion.com>
2015-12-09 10:54:32 -0500

    Add TypeScript type definitions

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+interface ColorPickerOptions {
+    color?: string | number;
+    background?: string | number;
+    el?: HTMLElement;
+    width?: number;
+    height?: number;
+}
+
+interface ColorPicker {
+    new (options?: ColorPickerOptions): ColorPicker;
+
+    appendTo(domElement: HTMLElement): void;
+
+    remove(): void;
+
+    setColor(color: string | number): void;
+
+    setSize(width: number, height: number): void;
+
+    setBackgroundColor(color: string | number): void;
+
+    setNoBackground(): void;
+
+    onChange(callback: Function): void;
+
+    getColor(): string | number;
+
+    getHexString(): string;
+
+    getHexNumber(): number;
+
+    getRGB(): { r: number, g: number, b: number };
+
+    getHSV(): { h: number, s: number, v: number };
+
+    isDark(): boolean;
+
+    isLight(): boolean;
+}
+
+declare var ColorPicker: ColorPicker;
+
+declare module "simple-color-picker" {
+    export = ColorPicker;
+}
