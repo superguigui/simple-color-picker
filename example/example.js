4a5965bcc3bcbabf27e5352f8719abe384c959ef
Eugene Cheung <ey.cheung@outlook.com>
2016-04-07 20:08:43 -0400

    Update dependencies and minor edits

--- example/example.js
+++ example/example.js
@@ -1,20 +1,17 @@
 'use strict';
 
-var domready = require('domready');
-
 var ColorPicker = require('../index.js');
 
-domready(function() {
-  /* --------------------------
-    ColorPicker
-  */
-  var colorPicker = new ColorPicker({
-    el: document.body,
-    color: '#123456',
-    background: '#656565'
-  });
-  colorPicker.onChange(function(hexStringColor) {
-    document.body.style.background = hexStringColor;
-    document.querySelector('h1 a').style.color = colorPicker.color.isDark() ? '#FFFFFF' : '#000000';
-  });
+/* --------------------------
+  ColorPicker
+*/
+var colorPicker = new ColorPicker({
+  el: document.body,
+  color: '#123456',
+  background: '#656565'
+});
+
+colorPicker.onChange(function(hexStringColor) {
+  document.body.style.background = hexStringColor;
+  document.querySelector('h1 a').style.color = colorPicker.color.isDark() ? '#FFFFFF' : '#000000';
 });
